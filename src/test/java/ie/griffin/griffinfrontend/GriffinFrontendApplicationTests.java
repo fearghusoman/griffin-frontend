33b218e09d012be0f5ab79f4523a9beca129d7b4
fearghusoman <fearghusoman@gmail.com>
2020-11-19 17:55:00 +0000

    Initial Commit

---
+++ src/test/java/ie/griffin/griffinfrontend/GriffinFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.griffin.griffinfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GriffinFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
